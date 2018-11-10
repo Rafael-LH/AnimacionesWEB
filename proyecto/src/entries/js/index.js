d72ae49fa678a3ea6da3fbdbfbf366a2e4359c57
Rafael Lopez <rafaellopez@MacBook-Pro-de-macbook.local>
2018-11-10 15:20:29 -0600

    save

--- proyecto/src/entries/js/index.js
+++ proyecto/src/entries/js/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {render} from 'react-dom'
 import HeaderContainer from '../../header/container/HeaderContainer'
+import {CSSTransitionGroup} from 'react-addons-css-transition-group'
 
 render(
     <HeaderContainer saludo='Hola mundo'/>,
